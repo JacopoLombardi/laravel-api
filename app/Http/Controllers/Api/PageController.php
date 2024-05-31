ea458f3fa678b895b09caf91630542cf30938c70
JacopoLombardi <lombajacopo@gmail.com>
2024-05-31 15:55:26 +0200

    [add] complete call api from back-end

---
+++ app/Http/Controllers/Api/PageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class PageController extends Controller
+{
+    public function index(){
+        $projects = Project::all();
+
+        return response()->json($projects);
+    }
+}

94f5368333c59675431dc94db8a7c1bcec8c81ec
JacopoLombardi <lombajacopo@gmail.com>
2024-06-04 09:29:20 +0200

    [add] added api Type & Technology

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
 
 class PageController extends Controller
 {
@@ -12,5 +14,19 @@ class PageController extends Controller
         $projects = Project::all();
 
         return response()->json($projects);
+    }
+
+
+    public function getTypes(){
+        $types = Type::all();
+
+        return response()->json($types);
+    }
+
+
+    public function getTechnologies(){
+        $technologies = Technology::all();
+
+        return response()->json($technologies);
     }
 }
